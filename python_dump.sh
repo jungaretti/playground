c97896fa84f12f070e3ed514f74cf139b7b76753
JP Ungaretti <hello@jungaretti.com>
2022-07-21 22:30:35 +0000

    Add python_dump

---
+++ python_dump.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "python"
+which python
+python --version
+
+echo "python3"
+which python3
+python3 --version
+
+echo "pip"
+which pip
+pip --version
+
+echo "pip3"
+which pip3
+pip3 --version
