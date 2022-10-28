932685d52622a4a27c6938c1de82a0f1eebdf87b
JP Ungaretti <git@jungaretti.com>
2022-10-28 20:49:30 +0000

    Add API route

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Capybara")
 
+class ApiHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Capybara API")
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/api", ApiHandler),
     ])
 
 async def main():
