b6a3d04d2ac97942708e429f047d997acad21133
JP Ungaretti <git@jungaretti.com>
2023-08-07 21:07:03 +0000

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-git submodule update
+UPDATE_PARAM=''
+if [ "$1" == "--update" ]; then
+    echo "Updating submodules"
+    UPDATE_PARAM='--remote'
+fi
+
+git submodule update --init --recursive $UPDATE_PARAM
 
 pushd devcontainers-features
 git checkout dotnet-install-script
